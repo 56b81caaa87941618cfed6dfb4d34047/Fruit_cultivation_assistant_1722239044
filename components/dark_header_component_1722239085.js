63fbe51a041d2396850de3e49191364e21c5ff72
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-29 07:44:45 +0000

    Replace placeholders across project

--- components/dark_header_component_1722239085.js
+++ components/dark_header_component_1722239085.js
@@ -12,7 +12,7 @@ Vue.component("dark_header_component_1722239085", {
                     <!-- Logo -->
                     <a class="flex items-center space-x-4" href="index.html" aria-label="Cruip">
                         <img id="site-logo" class="rounded-full" src="./images/logo.jpg" width="32" height="32" alt="Mary Rutt">
-                        <span id="site-name" :class="siteNameStyle">PERSON_NAME</span>
+                        <span id="site-name" :class="siteNameStyle">Juicy Jane</span>
                     </a>
                 </div>
                 <!-- Right side -->
